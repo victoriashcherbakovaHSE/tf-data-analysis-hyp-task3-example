05cdcae5671a61d1c9147001facabcce8bf52c36
Виктория Щербакова <96340050+victoriashcherbakovaHSE@users.noreply.github.com>
2023-04-12 13:05:37 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,14 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import ttest_1samp
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 871302863
 
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+def solution(x: np.array) -> bool:
+    alpha = 0.02
+    t_statistic, p_value = ttest_1samp(x, 300)
+    if p_value/2 < alpha and t_statistic > 0:
+        return True # отклоняем нулевую гипотезу
+    else:
+        return False # не отклоняем нулевую гипотезу
