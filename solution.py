a70c7aab90ecd9b8439dcd2c5f2e4e83cd599b9b
Виктория Щербакова <96340050+victoriashcherbakovaHSE@users.noreply.github.com>
2023-04-12 13:55:13 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,14 +1,17 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import ttest_1samp
+from scipy.stats import t
 
 
 chat_id = 871302863
 
 def solution(x: np.array) -> bool:
     alpha = 0.02
-    t_statistic, p_value = ttest_1samp(x, 300)
-    if p_value/2 < alpha and t_statistic > 0:
-        return True
-    else:
-        return False
+    mu=300
+    n = len(x)
+    x_bar = np.mean(x)
+    s = np.std(x, ddof=1)
+    se = s / np.sqrt(n)
+    t_stat = (x_bar - mu) / se
+    t_crit = t.ppf(alpha, n-1)
+    return t_stat < t_crit
